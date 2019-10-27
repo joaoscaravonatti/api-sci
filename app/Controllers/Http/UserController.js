54c143756d3a5571e014247ef6696e72c816fd7c
Jotadog <jvscaravonatti@gmail.com>
2019-10-27 19:43:46 -0300

    User controller

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,74 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use("App/Models/User");
+
+/**
+ * Resourceful controller for interacting with users
+ */
+class UserController {
+  /**
+   * Show a list of all users.
+   * GET users
+   *
+   * @param {object} ctx
+   * @param {Response} ctx.response
+   */
+  async index({ response }) {
+    return response.json(
+      await User.query()
+        .with("role")
+        .fetch()
+    );
+  }
+
+  /**
+   * Create/save a new user.
+   * POST users
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    const user = await User.create(request.all());
+
+    return response.json(user);
+  }
+
+  /**
+   * Display a single user.
+   * GET users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async show({ params, request, response }) {}
+
+  /**
+   * Update user details.
+   * PUT or PATCH users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {}
+
+  /**
+   * Delete a user with id.
+   * DELETE users/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {}
+}
+
+module.exports = UserController;
---
+++ database/migrations/1503250034280_user.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class UserSchema extends Schema {
+  up() {
+    this.create("users", table => {
+      table.increments();
+      table
+        .string("username", 80)
+        .notNullable()
+        .unique();
+      table
+        .string("email", 254)
+        .notNullable()
+        .unique();
+      table.string("password", 60).notNullable();
+      table.string("name", 255);
+      table
+        .integer("role_id")
+        .unsigned()
+        .references("id")
+        .inTable("roles");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("users");
+  }
+}
+
+module.exports = UserSchema;
